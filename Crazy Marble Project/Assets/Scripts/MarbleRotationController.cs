857421b27dac99d71464d4e48cd4b32816f9af1f
Penny-Dealer <97634906+Penny-Dealer@users.noreply.github.com>
2023-02-22 14:53:03 -0800

    (chore) added controls and map.

--- Crazy Marble Project/Assets/Scripts/MarbleRotationController.cs
+++ Crazy Marble Project/Assets/Scripts/MarbleRotationController.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.InputSystem.InputAction;
 
 public class MarbleRotationController : MonoBehaviour
 {
-    // Start is called before the first frame update
     [field: SerializeField]
-
     public Vector3 RotationVector { get; private set; }
+
+    private MarbleControls _marbleControls;
+
+    void Awake()
+    {
+        Debug.Log("I'm awake!");
+        _marbleControls = new MarbleControls();
+        _marbleControls.RotationControl.Enable();
+        _marbleControls.RotationControl.Rotate.performed += HandleRotateControl;
+        _marbleControls.RotationControl.Rotate.canceled += HandleRotateStop;
+    }
 
     void FixedUpdate()
     {
         Rigidbody body = GetComponent<Rigidbody>();
         body.AddTorque(RotationVector, ForceMode.VelocityChange);
     }
+
+    private void HandleRotateControl(CallbackContext context)
+    {
+        Vector2 rawInput = context.ReadValue<Vector2>();
+        RotationVector = new Vector3(rawInput.y, 0, -rawInput.x);
+    }
+
+    private void HandleRotateStop(CallbackContext context)
+    {
+        RotationVector = Vector3.zero;
+    }
+
 }
